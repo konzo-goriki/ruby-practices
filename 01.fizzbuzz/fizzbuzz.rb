a0af283e5871c48b006675ed2d02a6303f2ed2ab
konzo-goriki <konzo0418@gmail.com>
2021-08-31 03:20:54 +0900

    if文から"then"を消去
    fizzbuzz.rb内のif文から不要な"then"を消去した。

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -2,11 +2,11 @@ def fizzbuzz
   range = 1..20
 
   range.each do |n|
-    if n % 15 == 0 then
+    if n % 15 == 0
       puts "FizzBuzz"
-    elsif n % 5 == 0 then
+    elsif n % 5 == 0
       puts "Buzz"
-    elsif n % 3 == 0 then
+    elsif n % 3 == 0
       puts "Fizz"
     else
       puts n.to_s
