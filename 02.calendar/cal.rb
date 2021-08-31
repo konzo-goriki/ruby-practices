fb98792f0723db3f24c6454e08282e3947b4de68
konzo-goriki <konzo0418@gmail.com>
2021-08-31 23:47:42 +0900

    コマンドオプションをメソッド呼び出し側で処理
    cal.rbでカレンダー表示用メソッドで使用するオプション引数の読み込み処理を
    メソッド呼び出し側で処理するように変更し、不要なデフォルト引数を消去した。

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -14,21 +14,15 @@ def option
   options
 end
 
-def print_top_of_calendar(month=nil,year=nil)
-  month ||= Date.today.month
-  year ||= Date.today.year
-
+def print_top_of_calendar(month, year)
   puts "#{month}月 #{year}".center(20) #20:全角(2)*7 + 半角(1)*(7-1)
   print ["日","月","火","水","木","金","土"].join(' ')
   print("\n")
 end
 
-def print_body_of_calendar(month=nil,year=nil)
-  month ||= Date.today.month
-  year ||= Date.today.year
-
-  first_day = Date.new(year,month,1)
-  last_day = Date.new(year,month,-1)
+def print_body_of_calendar(month, year)
+  first_day = Date.new(year, month, 1)
+  last_day = Date.new(year, month, -1)
 
   print "　 " * first_day.wday
   (first_day..last_day).each do |day|
@@ -43,8 +37,8 @@ def print_body_of_calendar(month=nil,year=nil)
 end
 
 options = option
-month = options["-m"]&.to_i
-year = options["-y"]&.to_i
+month = options["-m"]&.to_i || Date.today.month
+year = options["-y"]&.to_i || Date.today.year
 print_top_of_calendar(month, year)
 print_body_of_calendar(month, year)
 
