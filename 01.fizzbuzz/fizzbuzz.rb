e7620a50fc67af4c88ea3c6d1400c55d55b4670b
konzo-goriki <konzo0418@gmail.com>
2021-09-02 11:10:49 +0900

    剰余チェックにzero?メソッドを使用するように修正
    剰余チェックで「== 0」を使用していた箇所で
    zero?メソッドを使用するように修正した。

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,9 +1,9 @@
 (1..20).each do |n|
-  if n % 15 == 0
+  if (n % 15).zero?
     puts "FizzBuzz"
-  elsif n % 5 == 0
+  elsif (n % 5).zero?
     puts "Buzz"
-  elsif n % 3 == 0
+  elsif (n % 3).zero?
     puts "Fizz"
   else
     puts n
