a4da3a7590f3d45f0e59faec94aea12fe8abc2a0
konzo-goriki <konzo0418@gmail.com>
2021-10-08 15:18:42 +0900

    スコア計算のバグを修正
    スコア計算のストライクのときの処理の代入順によるバグを修正した。

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -18,6 +18,7 @@ scores.each do |s|
     shots << s.to_i
   end
 end
+p shots
 
 # 全投球をフレームごとに分割
 # 10フレーム目だけ3投になる
@@ -29,28 +30,29 @@ if frames[10]
   frames[9] = frames[9] + frames[10]
   frames.delete_at(10)
 end
-
+p frames
 # 得点計算
 point = 0
 next1 = 0
 next2 = 0
 frames.reverse_each do |frame|
+  p point
   if frame.size == 3 # 3投してる10フレーム目
     point += frame.sum
-    next1 = frame[0]
     next2 = frame[1]
+    next1 = frame[0]
   elsif frame[0] == 10 # strike
     point += 10 + next1 + next2
-    next1 = 10
     next2 = next1
+    next1 = 10
   elsif frame.sum == 10 # spare
     point += 10 + next1
-    next1 = frame[0]
     next2 = frame[1]
+    next1 = frame[0]
   else
     point += frame.sum
-    next1 = frame[0]
     next2 = frame[1]
+    next1 = frame[0]
   end
 end
 
