9237b9e579df68ca3bed68dc3cd22a86cbd75455
konzo-goriki <konzo0418@gmail.com>
2021-10-08 14:49:49 +0900

    不適切な多重代入を修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -31,20 +31,26 @@ if frames[10]
 end
 
 # 得点計算
-point, next1, next2 = 0, 0, 0
+point = 0
+next1 = 0
+next2 = 0
 frames.reverse_each do |frame|
   if frame.size == 3 # 3投してる10フレーム目
     point += frame.sum
-    next1, next2 = frame[0], frame[1] 
+    next1 = frame[0]
+    next2 = frame[1]
   elsif frame[0] == 10 # strike
     point += 10 + next1 + next2
-    next1, next2 = 10, next1
+    next1 = 10
+    next2 = next1
   elsif frame.sum == 10 # spare
     point += 10 + next1
-    next1, next2 = frame[0], frame[1]
+    next1 = frame[0]
+    next2 = frame[1]
   else
     point += frame.sum
-    next1, next2 = frame[0], frame[1]
+    next1 = frame[0]
+    next2 = frame[1]
   end
 end
 
