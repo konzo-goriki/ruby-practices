a0d03651b1417d7770695bdf595e10a53fca4a2d
konzo-goriki <konzo0418@gmail.com>
2021-10-14 13:33:54 +0900

    next1,next2へのframeの代入を多重代入に修正
    スコア計算時のnext1,next2へのframeからの代入が複数行に渡っていたものを、
    多重を代入を利用して1行に修正した。

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -34,20 +34,17 @@ next2 = 0
 frames.reverse_each do |frame|
   if frame.size == 3 # 3投してる10フレーム目
     point += frame.sum
-    next2 = frame[1]
-    next1 = frame[0]
+    next1, next2 = frame
   elsif frame[0] == 10 # strike
     point += 10 + next1 + next2
     next2 = next1
     next1 = 10
   elsif frame.sum == 10 # spare
     point += 10 + next1
-    next2 = frame[1]
-    next1 = frame[0]
+    next1, next2 = frame
   else
     point += frame.sum
-    next2 = frame[1]
-    next1 = frame[0]
+    next1, next2 = frame
   end
 end
 
