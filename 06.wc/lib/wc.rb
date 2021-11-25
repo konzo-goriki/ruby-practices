a90713f76592e42a205d642ab8fccbea8f236d0b
konzo-goriki <konzo0418@gmail.com>
2021-11-25 11:29:57 +0900

    それぞれ存在した複数のパラメーターの計算用メソッドをひとつにまとめた
    count_lines, count_words, count_bytes
    をひとつのメソッドcount_paramsにまとめた。

--- 06.wc/lib/wc.rb
+++ 06.wc/lib/wc.rb
@@ -19,20 +19,13 @@ def parse_options
   options
 end
 
-def count_lines(file)
+def count_params(file, options)
   str = File.read(file)
-  str.lines.count
-end
-
-def count_words(file)
-  str = File.read(file)
-  ary = str.split(/\s+/)
-  ary.size
-end
-
-def count_bytes(file)
-  str = File.read(file)
-  str.bytesize
+  params = {}
+  params[:lines] = str.lines.count if options['-l']
+  params[:words] = str.split(/\s+/).size if options['-w']
+  params[:bytes] = str.bytesize if options['-c']
+  params
 end
 
 def show_stdin(options)
@@ -47,31 +40,29 @@ def show_total_params(total_params, options)
   print " #{total_params[:lines].to_s.rjust(7)}" if options['-l']
   print " #{total_params[:words].to_s.rjust(7)}" if options['-w']
   print " #{total_params[:bytes].to_s.rjust(7)}" if options['-c']
-  print ' total'
-  print "\n"
+  print " total\n"
 end
 
 def show_files(options)
-  patterns = ARGV
-  files = Dir.glob(patterns)
+  files = Dir.glob(ARGV)
 
-  total_params = { lines: 0, words: 0, bytes: 0 } if files.count >= 2
+  total_params = { lines: 0, words: 0, bytes: 0 }
 
   files.each do |file|
-    lines = count_lines(file)
-    print " #{lines.to_s.rjust(7)}" if options['-l']
-    words = count_words(file)
-    print " #{words.to_s.rjust(7)}" if options['-w']
-    bytes = count_bytes(file)
-    print " #{bytes.to_s.rjust(7)}" if options['-c']
-    print " #{file}"
-    print "\n"
-
-    next if files.count < 2
-
-    total_params[:lines] += lines
-    total_params[:words] += words
-    total_params[:bytes] += bytes
+    params = count_params(file, options)
+    if options['-l']
+      print " #{params[:lines].to_s.rjust(7)}"
+      total_params[:lines] += params[:lines]
+    end
+    if options['-w']
+      print " #{params[:words].to_s.rjust(7)}"
+      total_params[:words] += params[:words]
+    end
+    if options['-c']
+      print " #{params[:bytes].to_s.rjust(7)}"
+      total_params[:bytes] += params[:bytes]
+    end
+    print " #{file}\n"
   end
   show_total_params(total_params, options) if files.count >= 2
 end
